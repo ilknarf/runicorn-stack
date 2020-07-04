1a69ef13fd0f36f282a7f33fde49037a1ba4e549
Frank Li <lifrank@grinnell.edu>
2020-07-04 12:02:28 -0500

    add lambda

--- iam-role.tf
+++ iam-role.tf
@@ -1,3 +1,21 @@
+resource "aws_iam_role" "ssr_lambda" {
+  name_prefix = "runicorn-ssr-lambda-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_iam_role_policy_attachment" "ssr_attach" {
   role = aws_iam_role.ssr_lambda.name
   policy_arn = module.s3_static_read.arn
